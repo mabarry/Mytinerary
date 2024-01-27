50fccd08adb9f46e0c9560a6054d80e812c217c2
Alex Keener <alexkeener318@gmail.com>
2024-01-27 14:30:51 -0600

    Gets rough LandingPage look

---
+++ itinerary/src/LandingPage.js
@@ -0,0 +1,16 @@
+import "./LandingPage.css";
+function LandingPage() {
+    return(
+        <div className="main">
+            <div className="box">
+                <div className="logo"></div>
+                <span>Plan Your Trip Now</span>
+                <input className="dest" placeholder="Enter Destination..."></input>
+                <input className="date" placeholder="Enter Trip Dates..."></input>
+                <button className="submit">GO</button>
+            </div>
+        </div>
+    )
+};
+
+export default LandingPage;
