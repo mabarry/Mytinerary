8a7f737f6de35ea3f16c11183dce6f8438373d24
mabarry <mbarry4602@tamu.edu>
2024-01-27 20:50:28 -0600

    matthew changes

--- itinerary/src/LandingPage.js
+++ itinerary/src/LandingPage.js
@@ -23,11 +23,11 @@ function LandingPage() {
         </Helmet>
         <div className="main">  
             <div className="box">
-                <img src="./logo.png" height="100" width="100"/>
+                <img src="./logo.png" height="100" width="100" className="logo"/>
                 <span className="serviceName">Mytinerary</span>
                 <span className="text">Plan Your Trip Now</span>
                 <div className="dest-flex">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#2ac191" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="#2ac191" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
                     <input className="dest" placeholder="  Enter Destination..." />
                 </div>
                 <div className="date-flex">
